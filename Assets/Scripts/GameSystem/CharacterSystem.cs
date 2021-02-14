90369a30a59b84372454d6ee85bd1032fd29d23f
wxj <825754896@qq.com>
2021-02-14 16:12:46 +0800

    子系统

---
+++ Assets/Scripts/GameSystem/CharacterSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSystem :GameSystem
+{
+
+    private Character mainCharacter;
+    public static Dictionary<int,Character> npcCharacters;
+
+    public override void InitSystem()
+    {
+        base.InitSystem();
+    }
+
+    public override void OnSystemUpdate()
+    {
+        base.OnSystemUpdate();
+    }
+
+    public override void OnSystemStart()
+    {
+        base.OnSystemStart();
+    }
+
+    public override void OnSystemClose()
+    {
+        base.OnSystemClose();
+    }
+}
---
+++ Assets/Scripts/GameSystem/MessageSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageSystem :GameSystem
+{
+
+    private delegate void CharaterAtk();
+    private event CharaterAtk CharacterAtkHandler;
+
+    public override void InitSystem()
+    {
+        base.InitSystem();
+    }
+
+    public override void OnSystemStart()
+    {
+        base.OnSystemStart();
+    }
+
+    public override void OnSystemUpdate()
+    {
+        base.OnSystemUpdate();
+    }
+    public override void OnSystemClose()
+    {
+        base.OnSystemClose();
+    }
+
+    
+}
